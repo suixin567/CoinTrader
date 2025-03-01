8e4ba18dd54b6c7f1e81a81b03784d7110911d75
mashu <591821945@qq.com>
2025-03-01 11:05:51 +0800

    自动填写密码

--- CoinTrader.Forms/WinStartup.Designer.cs
+++ CoinTrader.Forms/WinStartup.Designer.cs
@@ -41,20 +41,20 @@
             // 
             // lblMessage
             // 
-            this.lblMessage.Location = new System.Drawing.Point(262, 52);
-            this.lblMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMessage.Location = new System.Drawing.Point(143, 30);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(387, 75);
+            this.lblMessage.Size = new System.Drawing.Size(211, 43);
             this.lblMessage.TabIndex = 0;
             this.lblMessage.Text = "正在初始化......";
             this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // btnRetry
             // 
-            this.btnRetry.Location = new System.Drawing.Point(346, 158);
-            this.btnRetry.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRetry.Location = new System.Drawing.Point(189, 90);
+            this.btnRetry.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnRetry.Name = "btnRetry";
-            this.btnRetry.Size = new System.Drawing.Size(189, 51);
+            this.btnRetry.Size = new System.Drawing.Size(103, 29);
             this.btnRetry.TabIndex = 1;
             this.btnRetry.Text = "重 试";
             this.btnRetry.UseVisualStyleBackColor = true;
@@ -66,9 +66,8 @@
             this.pnlStart.Controls.Add(this.btnRetry);
             this.pnlStart.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlStart.Location = new System.Drawing.Point(0, 0);
-            this.pnlStart.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.pnlStart.Name = "pnlStart";
-            this.pnlStart.Size = new System.Drawing.Size(847, 247);
+            this.pnlStart.Size = new System.Drawing.Size(462, 141);
             this.pnlStart.TabIndex = 2;
             // 
             // pnlLogin
@@ -78,28 +77,25 @@
             this.pnlLogin.Controls.Add(this.txtPwd);
             this.pnlLogin.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlLogin.Location = new System.Drawing.Point(0, 0);
-            this.pnlLogin.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.pnlLogin.Name = "pnlLogin";
-            this.pnlLogin.Size = new System.Drawing.Size(847, 247);
+            this.pnlLogin.Size = new System.Drawing.Size(462, 141);
             this.pnlLogin.TabIndex = 3;
             this.pnlLogin.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlLogin_Paint);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(218, 105);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(119, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 21);
+            this.label1.Size = new System.Drawing.Size(53, 12);
             this.label1.TabIndex = 4;
             this.label1.Text = "登录密码";
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(516, 97);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.btnLogin.Location = new System.Drawing.Point(281, 55);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(101, 37);
+            this.btnLogin.Size = new System.Drawing.Size(55, 21);
             this.btnLogin.TabIndex = 3;
             this.btnLogin.Text = "确 定";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -107,23 +103,23 @@
             // 
             // txtPwd
             // 
-            this.txtPwd.Location = new System.Drawing.Point(324, 100);
-            this.txtPwd.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.txtPwd.Location = new System.Drawing.Point(177, 57);
             this.txtPwd.Name = "txtPwd";
             this.txtPwd.PasswordChar = '*';
-            this.txtPwd.Size = new System.Drawing.Size(180, 31);
+            this.txtPwd.Size = new System.Drawing.Size(100, 21);
             this.txtPwd.TabIndex = 2;
+            this.txtPwd.Text = "mashu567";
             this.txtPwd.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtPwd_KeyUp);
             // 
             // WinStartup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(847, 247);
+            this.ClientSize = new System.Drawing.Size(462, 141);
             this.Controls.Add(this.pnlLogin);
             this.Controls.Add(this.pnlStart);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "WinStartup";
