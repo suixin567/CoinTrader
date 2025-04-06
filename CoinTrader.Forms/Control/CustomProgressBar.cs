102993abdb46d2b56fd827acfd84dabd9185fa7d
mashu <591821945@qq.com>
2025-04-06 20:38:06 +0800

    CustomProgressBar支持向左模式

--- CoinTrader.Forms/Control/CustomProgressBar.cs
+++ CoinTrader.Forms/Control/CustomProgressBar.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace CoinTrader.Forms.Control
 {
     public partial class CustomProgressBar : UserControl
     {
-        private float _minimum = 1.4f;
-        private float _maximum = 2.5f;
-        private float _value = 1.4f;
+        private float _minimum = 0f;
+        private float _maximum = 100f;
+        private float _value = 0f;
+
+        public enum ProgressDirection
+        {
+            LeftToRight,
+            RightToLeft
+        }
+
+        /// <summary>
+        /// 控制进度条的填充方向  
+        /// LeftToRight 模式下：左侧为 Minimum，右侧为 Maximum，进度条从左向右填充  
+        /// RightToLeft 模式下：坐标轴依然为左侧 Minimum、右侧 Maximum，但进度条填充从右向左，
+        /// 即当 Value 越低时，填充越多，表示从最大值“回落”到最小值。
+        /// </summary>
+        public ProgressDirection Direction { get; set; } = ProgressDirection.LeftToRight;
 
         public float Minimum
         {
@@ -56,37 +69,57 @@ namespace CoinTrader.Forms.Control
 
         public CustomProgressBar()
         {
-            this.DoubleBuffered = true;
-            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.ResizeRedraw | ControlStyles.OptimizedDoubleBuffer, true);
-            this.Height = 60;
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.ResizeRedraw | ControlStyles.OptimizedDoubleBuffer, true);
+            Height = 60;
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
             var g = e.Graphics;
-            g.Clear(this.BackColor);
+            g.Clear(BackColor);
 
-            var barRect = new Rectangle(10, 25, this.Width - 20, 10);
+            // 预留左右边距10像素，上边距25像素，绘制区域高度10像素
+            var barRect = new Rectangle(10, 25, Width - 20, 10);
 
-            // 背景条
+            // 绘制背景条
             using (var bgBrush = new SolidBrush(Color.LightGray))
                 g.FillRectangle(bgBrush, barRect);
 
-            // 当前进度条
-            float percent = (Value - Minimum) / (Maximum - Minimum);
-            int progressWidth = (int)(barRect.Width * percent);
-            var progressRect = new Rectangle(barRect.X, barRect.Y, progressWidth, barRect.Height);
+            // 计算进度条填充
+            int progressWidth;
+            Rectangle progressRect;
+
+            if (Direction == ProgressDirection.LeftToRight)
+            {
+                // 正常模式：进度条从左向右填充
+                float percent = (Value - Minimum) / (Maximum - Minimum);
+                progressWidth = (int)(barRect.Width * percent);
+                progressRect = new Rectangle(barRect.X, barRect.Y, progressWidth, barRect.Height);
+            }
+            else
+            {
+                // 右向左模式：进度条填充从右侧向左扩展
+                // 当 Value == Maximum 时，填充宽度为0；当 Value == Minimum 时，填充满整个 barRect
+                float percent = (Maximum - Value) / (Maximum - Minimum);
+                progressWidth = (int)(barRect.Width * percent);
+                int x = barRect.X + barRect.Width - progressWidth;
+                progressRect = new Rectangle(x, barRect.Y, progressWidth, barRect.Height);
+            }
 
             using (var progressBrush = new SolidBrush(Color.DodgerBlue))
                 g.FillRectangle(progressBrush, progressRect);
 
-            // 绘制标记
+            // 绘制标记（标记位置采用统一映射：左侧为 Minimum，右侧为 Maximum）
             foreach (var marker in _markers)
             {
-                if (marker.Position < Minimum || marker.Position > Maximum) continue;
+                if (marker.Position < Minimum || marker.Position > Maximum)
+                    continue;
 
-                float x = barRect.X + ((marker.Position - Minimum) / (Maximum - Minimum)) * barRect.Width;
+                float normalized = (marker.Position - Minimum) / (Maximum - Minimum);
+                // 无论方向如何，坐标轴保持一致
+                float x = barRect.X + normalized * barRect.Width;
 
                 using (var pen = new Pen(Color.Red, 1) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dash })
                     g.DrawLine(pen, x, barRect.Top - 12, x, barRect.Bottom + 15);
@@ -98,12 +131,9 @@ namespace CoinTrader.Forms.Control
                     {
                         var size = g.MeasureString(marker.TopLabel, font);
                         float labelX = x - size.Width / 2;
-
-                        // 保证不超出控件边界
                         if (labelX < 0) labelX = 0;
-                        if (labelX + size.Width > this.Width)
-                            labelX = this.Width - size.Width;
-
+                        if (labelX + size.Width > Width)
+                            labelX = Width - size.Width;
                         g.DrawString(marker.TopLabel, font, textBrush, labelX, barRect.Top - size.Height - 5);
                     }
 
@@ -111,25 +141,13 @@ namespace CoinTrader.Forms.Control
                     {
                         var size = g.MeasureString(marker.BottomLabel, font);
                         float labelX = x - size.Width / 2;
-
-                        // 保证不超出控件边界
                         if (labelX < 0) labelX = 0;
-                        if (labelX + size.Width > this.Width)
-                            labelX = this.Width - size.Width;
-
+                        if (labelX + size.Width > Width)
+                            labelX = Width - size.Width;
                         g.DrawString(marker.BottomLabel, font, textBrush, labelX, barRect.Bottom + 5);
                     }
                 }
             }
-
-
-            // 当前值
-            //using (var textBrush = new SolidBrush(Color.Black))
-            //using (var font = new Font("Arial", 8, FontStyle.Bold))
-            //{
-            //    string text = $"{Value:F2}";
-            //    g.DrawString(text, font, textBrush, barRect.Right + 5, barRect.Top - 2);
-            //}
         }
     }
 }
